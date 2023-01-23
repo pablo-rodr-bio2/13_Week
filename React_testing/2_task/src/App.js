f3a8b0438b889cdde9b4e7edb85f1b2bcce56cbf
pablo-rodr-bio2 <pablo.rodriguez@toptal.com>
2023-01-23 16:08:06 +0100

    Fix Unit Testing tasks

---
+++ React_testing/2_task/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Light from "./components/Light";
+import "./styles.css";
+
+const colors = ['red', 'yellow', 'blue', 'green']
+
+const colorsMap = [...colors, ...colors]
+
+export default function App() {
+
+  const [isTurnedOn, setIsTurnedOn] = useState(true)
+
+  const handleTurn = () => {
+    setIsTurnedOn((value) => !value)
+  }
+ 
+
+  return (
+    <main>
+      <section className="lights">
+
+        {
+          colorsMap.map((color, index) =>
+                      <Light key={index} color={isTurnedOn ? color : 'off'} />)
+        }
+
+      </section>
+
+      <section className="buttons">
+        <button onClick={handleTurn}>{isTurnedOn ? 'Turn off' : 'Turn on'}</button>
+      </section>
+
+    </main>
+  );
+}
--- Unit_Testing/index.test.js
+++ Unit_Testing/index.test.js
@@ -1,48 +1,50 @@
 const isLeapYear = require('./index')
 
-//TODO
-// describe('isLeapYear')
-// add 'describe' for 'if's and 'when's
-// describe(when the user is defined)
-// it(does something)
-// trying to get 2 lvls of describe
-
 describe('isLeapYear ', () => {
-    describe('This are leap years: 2012, 2008, 2004, 1996, 2000, 1600, 1200', () => {
+    describe('when the year is divisible by 4 and not 100 ', () => {
     
-        it('is be divisible by 4 and not by 100', () => {
+        it('returns true', () => {
             expect(isLeapYear(2012)).toBe(true)
             expect(isLeapYear(2008)).toBe(true)
             expect(isLeapYear(2004)).toBe(true)
             expect(isLeapYear(1996)).toBe(true)
         })
-    
-        it('is be divisible by 400', () => {
+    })
+
+    describe('when the year is divisible by 400', () => {
+        it('returns true', () => {
             expect(isLeapYear(2000)).toBe(true)
             expect(isLeapYear(1600)).toBe(true)
             expect(isLeapYear(1200)).toBe(true)
         })
     })
     
-    describe('This are not leap years: 1700, 1900, 2600, 1701, 1910, 2002', () => {
+    describe('when the year is not divisible by 400', () => {
         
-        it('is not divisible by 400', () => {
+        it('returns false', () => {
             expect(isLeapYear(1700)).toBe(false)
             expect(isLeapYear(1900)).toBe(false)
             expect(isLeapYear(2600)).toBe(false)
         })
-    
-        it('is not divisible by 4', () => {
+    })
+
+    describe('when the year is not divisible by 4', () => {
+        it('returns false', () => {
             expect(isLeapYear(1701)).toBe(false)
             expect(isLeapYear(1910)).toBe(false)
             expect(isLeapYear(2002)).toBe(false)
         })
     })
     
-    describe('This are bad arguments', () => {
-        it('is not a number', () => {
+    describe('when the argument passed is not defined', () => {
+        it('throws an error', () => {
             expect(() => isLeapYear()).toThrow('Your year is not defined')
             expect(() => isLeapYear(undefined)).toThrow()
+        })
+    })
+
+    describe('when the year passed is a string', () => {
+        it('throws an error', () => {
             expect(() => isLeapYear('year')).toThrow('Your year is not a number')
         })
     })
